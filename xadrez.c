a33d86bf8969be49a54edf3cb11dde499bfe2204
KleberUchoa <150630675+KleberUchoa@users.noreply.github.com>
2025-07-28 22:35:11 +0000

    Ajustes de visualização dos movimentos

--- xadrez.c
+++ xadrez.c
@@ -4,31 +4,34 @@ void MoveTorre(int casas)
 {
     if(casas > 0)
     {
-        printf("Direita\n");
         MoveTorre(casas - 1);
+        printf("%dª casa à Direita\n", casas);
+       
     }
 }
 void MoveDama(int casas)
 {
     if(casas > 0)
     {
-        printf("Esquerda\n");
         MoveDama(casas - 1);
+        printf("%dª casa à esquerda\n", casas);
     }
 }
 void MoveBispo(int casas)
 {
     if(casas > 0)
     {
+        MoveBispo(casas - 1);
+        printf("%dª casa\n", casas);
         printf("Cima\n");
         printf("Direita\n");
-        MoveBispo(casas - 1);
     }
 }
 void MoveCavalo(int vezes)
 {
     for(int i = 0; i < vezes; i++)
     {
+        printf("%dº movimento\n", vezes);
         printf("Baixo\n");
         printf("Baixo\n");
         printf("Esquerda\n");
@@ -44,7 +47,7 @@ int main()
     MoveDama(8);
     printf("Movendo o bispo 5 casas para cima e à direita\n");
     MoveBispo(5);
-    printf("Movendo o cavalo 1 vez para 2 casas para baixa e 1 casa a esquerda\n");
+    printf("Movendo o cavalo 1 vez: 2 casas para baixo e 1 casa a esquerda\n");
     MoveCavalo(1);
     return 0;
 
